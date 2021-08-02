7b9bc03b4907b9e4dfcca67be73303bb643c31d5
matt <mattk311@gmail.com>
2021-08-02 06:23:19 -0700

    Add project files.

---
+++ EncryptDecryptPassword/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace EncryptDecryptPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.EnvVariable = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(99, 166);
+            this.btnEncrypt.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(391, 32);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(99, 217);
+            this.btnDecrypt.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(391, 29);
+            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // EnvVariable
+            // 
+            this.EnvVariable.Location = new System.Drawing.Point(99, 44);
+            this.EnvVariable.Margin = new System.Windows.Forms.Padding(2);
+            this.EnvVariable.Name = "EnvVariable";
+            this.EnvVariable.Size = new System.Drawing.Size(392, 23);
+            this.EnvVariable.TabIndex = 2;
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(99, 98);
+            this.Password.Margin = new System.Windows.Forms.Padding(2);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.PlaceholderText = "******";
+            this.Password.Size = new System.Drawing.Size(392, 23);
+            this.Password.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Environment Variable Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 270);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.EnvVariable);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Encrypt Decrypt Environment Variables";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox EnvVariable;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EncryptDecryptPassword/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptDecryptPassword
+{
+    public partial class Form1 : Form
+    {
+
+        static string key { get; set; } = "M@ttK3nn3dy";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static string Encrypt(string text)
+        {
+            try
+            {
+                using (var md5 = new MD5CryptoServiceProvider())
+                {
+                    using (var tdes = new TripleDESCryptoServiceProvider())
+                    {
+                        tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                        tdes.Mode = CipherMode.ECB;
+                        tdes.Padding = PaddingMode.PKCS7;
+
+                        using (var transform = tdes.CreateEncryptor())
+                        {
+                            byte[] textBytes = UTF8Encoding.UTF8.GetBytes(text);
+                            byte[] bytes = transform.TransformFinalBlock(textBytes, 0, textBytes.Length);
+                            return Convert.ToBase64String(bytes, 0, bytes.Length);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error : " + ex.ToString();
+            }
+
+        }
+
+        public static string Decrypt(string cipher)
+        {
+            try
+            {
+                using (var md5 = new MD5CryptoServiceProvider())
+                {
+                    using (var tdes = new TripleDESCryptoServiceProvider())
+                    {
+                        tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                        tdes.Mode = CipherMode.ECB;
+                        tdes.Padding = PaddingMode.PKCS7;
+
+                        using (var transform = tdes.CreateDecryptor())
+                        {
+                            byte[] cipherBytes = Convert.FromBase64String(cipher);
+                            byte[] bytes = transform.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+                            return UTF8Encoding.UTF8.GetString(bytes);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+
+
+
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(Password.Text))
+            {
+                MessageBox.Show("Password cannot be empty");
+            }
+            else if (String.IsNullOrEmpty(EnvVariable.Text))
+            {
+                MessageBox.Show("Environment variable name cannot be empty");
+            }
+            else
+            {
+                string encryptedPassword = Encrypt(Password.Text);
+                System.Environment.SetEnvironmentVariable(EnvVariable.Text, encryptedPassword, EnvironmentVariableTarget.Machine);
+                MessageBox.Show("Password set for environment variable " + EnvVariable.Text);
+            }
+
+
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(EnvVariable.Text))
+            {
+                MessageBox.Show("Environment variable name cannot be empty");
+            }
+            else
+            {
+                string envVar = System.Environment.GetEnvironmentVariable(EnvVariable.Text, EnvironmentVariableTarget.Machine);
+                string decryptedPassword = Decrypt(envVar);
+                MessageBox.Show(EnvVariable.Text + " decrypted password is " + decryptedPassword);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
